0750b7b656a5cca01967f28e1f5335d79846f348
Coder11332 <pranav.a.murthy@gmail.com>
2021-10-24 12:59:43 -0700

    Implemented drive P-loops.

--- src/main.cpp
+++ src/main.cpp
@@ -65,10 +65,16 @@ const int heights2[NUM_HEIGHTS] = {0, 700,1800};
 void opcontrol() {
 	Clamp.tare_position();
   int goalHeight = 0;
+	double prevr = 0;
+	double prevl = 0;
   while (true){
 		double power = control.get_analog(ANALOG_LEFT_Y);
 		double turn = control.get_analog(ANALOG_LEFT_X);
-		driverControl(2*power+turn, 2*power-turn);
+		double right = (2*power + turn + prevr)/2;
+		double left = (2*power - turn + prevr)/2;
+		prevr = right;
+		prevl = left;
+		driverControl(prevr, 2*power-turn);
 		if (up.changedToPressed()){
 			Clamp.move_absolute(800, 100);
 		}
