6ddbedb8b35f6b9a78862961ac41c797d0e133b7
b-i-n-g-l-o <93052726+b-i-n-g-l-o@users.noreply.github.com>
2021-11-18 15:15:06 -0800

    messed up functions; fixed some auton

--- include/autonomous.h
+++ include/autonomous.h
@@ -116,41 +116,40 @@ void AWP1(){
       .withOutput(driveauton)
       .buildMotionProfileController();
       
-    Clamp.move_relative(1000, 200);
+    Clamp.move_relative(500, 100);
     delay(100);
     driverControl(-100, -100);
      delay(1000);
-    Clamp.move_relative(-1000, 100);
+    Clamp.move_relative(-500, 100);
+    delay(15000);
 }
 void AWP2(){
 
-  std::shared_ptr<ChassisController> driveauton =
-  ChassisControllerBuilder()
-  .withMotors({FLPort,BLPort},{FRPort,BRPort})
-  .withGains(
-  {0.002, 0, 0.0001}, // Distance controller gains
-  {0.001, 0, 0.0001} // Turn controller gains
-  )
-  .withMaxVelocity(200)
-
-  .withDerivativeFilters(
-        std::make_unique<AverageFilter<3>>()
+std::shared_ptr<ChassisController> driveauton =
+    ChassisControllerBuilder()
+    .withMotors({FLPort,BLPort},{FRPort,BRPort})
+    .withGains(
+    {0.002, 0, 0.0001}, // Distance controller gains
+    {0.001, 0, 0.0001} // Turn controller gains
     )
-  // green gearset, 4 inch wheel diameter, 15 inch wheel track
-  .withDimensions(AbstractMotor::gearset::green, {{4_in, 15_in}, imev5GreenTPR})
-  .withOdometry() // use the same scales as the chassis (above)
-  .buildOdometry(); // build an odometry chassis
+    .withMaxVelocity(150)
+    .withDerivativeFilters(
+          std::make_unique<AverageFilter<3>>()
+      )
+    // green gearset, 4 inch wheel diameter, 15 inch wheel track
+    .withDimensions(AbstractMotor::gearset::green, {{4_in, 16_in}, imev5GreenTPR})
+    .withOdometry() // use the same scales as the chassis (above)
+    .buildOdometry(); // build an odometry chassis
 
   std::shared_ptr<ChassisController> driveautonnotpid =
   ChassisControllerBuilder()
   .withMotors({FLPort,BLPort},{FRPort,BRPort})
-  .withMaxVelocity(200)
-
+  .withMaxVelocity(120)
   // green gearset, 4 inch wheel diameter, 15 inch wheel track
-  .withDimensions(AbstractMotor::gearset::green, {{4_in, 15_in}, imev5GreenTPR})
+  .withDimensions(AbstractMotor::gearset::green, {{4_in, 16_in}, imev5GreenTPR})
   .build(); // build an odometry chassis
 
-  std::shared_ptr<AsyncMotionProfileController> profileController2 =
+  std::shared_ptr<AsyncMotionProfileController> profileController =
   AsyncMotionProfileControllerBuilder()
     .withLimits({
       1.0, // Maximum linear velocity of the Chassis in m/s
@@ -159,14 +158,25 @@ void AWP2(){
     })
     .withOutput(driveauton)
     .buildMotionProfileController();
-    driveauton->moveDistance(25_in);
-    Clamp.move_relative(5000, 10);
-    delay(1000);
-    driveauton->moveDistance(-20_in);
-    delay(300);
-    Clamp.move_relative(-1000,50);
-    delay(200);
-    driveauton->turnAngle(180_deg);
+
+  // Target location path
+  profileController->generatePath({
+      {25_in, 0_in, 0_deg},
+      {0_ft, 0_ft, 0_deg}},
+      "first_move"
+  );
+  profileController->generatePath({
+      {0_in, 0_in, 0_deg},
+      {-20_ft, 0_ft, 0_deg}},
+      "retreat"
+  );
+
+profileController->setTarget("first_move",true);
+Clamp.move_relative(1000, 100);
+   delay(1000);
+profileController->setTarget("retreat", true);
+Clamp.move_relative(-1000,100);
+delay(15000);
 
 
 }
@@ -216,16 +226,17 @@ void TEST_GO_1() {
       "first_move"
   );
   profileController->generatePath({
-      {50_in, 0_in, 0_deg},
+      {-50_in, 0_in, 0_deg},
       {0_ft, 0_ft, 0_deg}},
       "retreat"
   );
 
 profileController->setTarget("first_move",true);
 delay(10);
-Clamp.move_relative(2500, 50);
+Clamp.move_relative(1000, 200);
 delay(6000);
-profileController->setTarget("first_move",false);
+profileController->setTarget("retreat", true);
+delay(15000);
 
 
 
@@ -274,20 +285,28 @@ void TEST_GO_2() {
   );
   profileController->generatePath({
       {0_ft, 0_ft, 0_deg},
-      {50_in, 0_in, 0_deg}},
-      "run_home"
+      {36_in, 0_in, 0_deg}},
+      "move_second_numogo"
+  );
+    profileController->generatePath({
+      {0_ft, 0_ft, 0_deg},
+      {-20_in, 0_in, 0_deg}},
+      "home"
   );
 
 profileController->setTarget("first_move",true);
-GHold.move_relative(7500, 10);
-delay(3000);
+GHold.move_relative(1000, 100);
+delay(2000);
 driveauton->turnAngle(90_deg);
 delay(500);
-driveauton->moveDistance(36_in);
+profileController->setTarget("move_second_numogo", true);
 delay(300);
 driveauton->turnAngle(270_deg);
-Clamp.move_relative(5000,10);
-driveauton->moveDistance(-20_in);
+delay(1000);
+Clamp.move_relative(-1000,100);
+delay(500);
+profileController->setTarget("home", true);
+delay(15000);
 }
 
 void TEST_GO_3() {
@@ -327,26 +346,39 @@ void TEST_GO_3() {
 
   // Target location path
   profileController->generatePath({
-      {54_in, 0_in, 0_deg},
+      {72_in, 0_in, 0_deg},
       {0_ft, 0_ft, 0_deg}},
       "first_move"
   );
   profileController->generatePath({
       {0_ft, 0_ft, 0_deg},
-      {50_in, 0_in, 0_deg}},
-      "run_home"
+      {24_in, 0_in, 0_deg}},
+      "backout"
+  );
+    profileController->generatePath({
+      {0_ft, 0_ft, 0_deg},
+      {24_in, 0_in, 0_deg}},
+      "go_2_WP"
+  );
+      profileController->generatePath({
+      {0_ft, 0_ft, 0_deg},
+      {-20_in, 0_in, 0_deg}},
+      "retreat_WP"
   );
 
-GHold.move_relative(2500, 10);
+GHold.move_relative(1000, 150);
 delay(1000);
-
-driveauton->moveDistance(72_in);
-GHold.move_relative(-2500,10);
+profileController->setTarget("first_move", true);
+delay(300);
+GHold.move_relative(-1000,200);
 delay(100);
-driveauton->moveDistance(-24_in);
+profileController->setTarget("backout", true);
 driveauton->turnAngle(225_deg);
 delay(500);
-driveauton->moveDistance(27_in);
-Clamp.move_relative(5000,10);
-driveauton->moveDistance(20_in);
+profileController->setTarget("go_2_WP", true);
+delay(1000);
+Clamp.move_relative(1000,150);
+profileController->setTarget("retreat_WP", true);
+Clamp.move_relative(-1000,150);
+delay(15000);
 }
